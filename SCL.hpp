d12308906da62dd9b68eb56791f3cb6735a705a6
Wiktor Nowicki <59111798+wiktornowicki@users.noreply.github.com>
2019-12-28 12:39:35 +0100

    indent

--- SCL.hpp
+++ SCL.hpp
@@ -3,13 +3,38 @@
 
 namespace scl
 {
-	long absolute(const long n);
-	bool isEven(const long n);
-	double addition(double a, double b);
-	double substraction(double a, double b);
-	double multiplication(double a, double b);
-	double division(double a, double b);
-	double power(long base, long exponent);
+	long absolute(
+		const long n
+	);
+	
+	bool isEven(
+		const long n
+	);
+	
+	double addition(
+		double a,
+		double b
+	);
+	
+	double substraction(
+		double a,
+		double b
+	);
+	
+	double multiplication(
+		double a,
+		double b
+	);
+	
+	double division(
+		double a,
+		double b
+	);
+	
+	double power(
+		long base,
+		long exponent
+	);
 }
 
 #endif
